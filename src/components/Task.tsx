7a77e2d1f7dad3404aae20e7671f82938448f1ac
raphaelc484 <raphaelc484@hotmail.com>
2022-07-19 14:45:34 -0300

    testando problema com renderização de sem tarefas

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,21 +1,33 @@
 import styles from "./Task.module.css";
 import { Trash } from "phosphor-react";
-import { ChangeEventHandler } from "react";
+import { ChangeEvent } from "react";
+
+interface StatusProps {
+  id: string;
+  statusChange: boolean;
+}
 
 interface TaskProps {
   id: string;
   task: string;
   onDeleteTask: (taskId: string) => void;
+  onChangeStatus: ({ id, statusChange }: StatusProps) => void;
 }
 
-export function Task({ id, task, onDeleteTask }: TaskProps) {
+export function Task({ id, task, onDeleteTask, onChangeStatus }: TaskProps) {
   function handleDeleteTask() {
     onDeleteTask(id);
   }
 
+  function teste(event: ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.checked);
+
+    onChangeStatus({ id, statusChange: event.target.checked });
+  }
+
   return (
     <div className={styles.task}>
-      <input id={id} type="checkbox" />
+      <input id={id} type="checkbox" onChange={teste} />
       <label htmlFor={id}>
         {task}
         <button onClick={handleDeleteTask}>
--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -4,16 +4,24 @@ import { useState } from "react";
 import { Task } from "./Task";
 import { NewTask } from "./NewTask";
 
+interface StatusProps {
+  id: string;
+  statusChange: boolean;
+}
+
+interface TasksProps {
+  id: string;
+  content: string;
+  check: boolean;
+}
+
 export function TaskBoard() {
-  const [tasks, setTasks] = useState([
-    { id: "1", content: "Oi" },
-    { id: "2", content: "Teste" },
-  ]);
+  const [tasks, setTasks] = useState<TasksProps[]>([]);
 
   function createTask(taskToCreate: string) {
     setTasks([
       ...tasks,
-      { id: String(tasks.length + 1), content: taskToCreate },
+      { id: String(tasks.length + 1), content: taskToCreate, check: false },
     ]);
   }
 
@@ -25,6 +33,17 @@ export function TaskBoard() {
     setTasks(tasksWithoutDeleteChoose);
   }
 
+  function changeStatusTask({ id, statusChange }: StatusProps) {
+    const tasksWithStatusChange = [...tasks];
+    const findIndex = tasks.findIndex((item) => item.id === id);
+    tasksWithStatusChange[findIndex].check = statusChange;
+
+    setTasks(tasksWithStatusChange);
+  }
+
+  const t = tasks.filter((item) => item.check === true);
+  const t2 = tasks.length;
+
   return (
     <div>
       <NewTask onCreate={createTask} />
@@ -33,28 +52,34 @@ export function TaskBoard() {
           Tarefas criadas <b>{tasks.length}</b>
         </strong>
         <strong>
-          Concluídas <b>0</b>
+          Concluídas{" "}
+          <b>
+            {t.length} de {tasks.length}
+          </b>
         </strong>
       </div>
       <ul className={styles.tasks}>
         {tasks.map((task) => {
-          if (!task) {
-            return (
-              <div className={styles.taskBoardEmpty}>
-                <img src={clipboardImg} />
-                <strong>Você ainda não tem tarefas cadastradas</strong>
-                <p>Crie tarefas e organize seus itens a fazer</p>
-              </div>
-            );
-          } else {
+          if (task) {
+            console.log(task);
             return (
               <li key={task.id}>
                 <Task
                   id={task.id}
                   task={task.content}
                   onDeleteTask={deleteTask}
+                  onChangeStatus={changeStatusTask}
                 />
               </li>
+            );
+          } else {
+            console.log("aqui");
+            return (
+              <div className={styles.taskBoardEmpty}>
+                <img src={clipboardImg} />
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+              </div>
             );
           }
         })}
